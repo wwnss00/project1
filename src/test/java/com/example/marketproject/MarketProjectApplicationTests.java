a61d4c5de8253dcebefd33beab9c6484d3856bf3
wwnss00 <wns208500@gmail.com>
2025-11-18 21:58:20 +0900

    프로젝트 생성

---
+++ src/test/java/com/example/marketproject/MarketProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marketproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

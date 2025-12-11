a8d7540e37e5502056db71d12f5edd1c08cbe5db
wwnss00 <wns208500@gmail.com>
2025-12-11 17:40:19 +0900

    feat:like엔티티 생성

--- src/main/java/com/example/marketproject/domain/entity/Like.java
+++ src/main/java/com/example/marketproject/domain/entity/Like.java
@@ -1,19 +1,37 @@
 package com.example.marketproject.domain.entity;
 
+import com.example.marketproject.domain.common.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "like")
+@Table(name = "likes",
+            uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_user_post",
+                        columnNames = {"user_id", "post_id"}
+                )
+            }
+            )
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Like {
+public class Like extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+
 
     
 }

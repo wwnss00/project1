786c1271e34ac5dfd6d3a0142a800c76a87b1c0c
wwnss00 <wns208500@gmail.com>
2026-01-07 21:30:08 +0900

    feat:UserRepository 생성

--- src/main/java/com/example/marketproject/repository/UserRepository.java
+++ src/main/java/com/example/marketproject/repository/UserRepository.java
@@ -4,10 +4,16 @@ import com.example.marketproject.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    //회원가입
-    boolean
+    Optional<User> findByLoginId(String loginId);
 
+    boolean existsByLoginId(String loginId);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
 }
